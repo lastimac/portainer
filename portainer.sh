5dc41742e273107f1528431e1b561d5f1e36ef99
ibuler <ibuler@qq.com>
2024-08-27 18:24:41 +0800

    perf: add start

---
+++ portainer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock  \
+  -v /data:/data  \
+  portainer/portainer-ce:latest
